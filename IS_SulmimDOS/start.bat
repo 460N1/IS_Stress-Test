6c83d5c13e8bae131b3da8d6b7752c2ffd36384d
460N1 <agon.hoxha@uni-pr.edu>
2019-01-16 20:04:40 +0100

    |

--- IS_SulmimDOS/start.bat
+++ IS_SulmimDOS/start.bat
@@ -8,17 +8,17 @@ REM \**************************/
 
 	START cmd /c python ISStress.py getintopc.com -q
 
-REM --- Per ndihme, ekzekuto sulmo.py --help nga cmd.
+REM Per ndihme, ekzekuto sulmo.py --help nga cmd.
 
 	RawCap.exe 4 Vertetimi.pcap
 
-REM --- Percakton network interface qe do t'e sniff RawCap.exe
-REM --- Ne nje host tjeter, kjo do mund te jete ndryshe.
-REM --- Percakton emrin e file ku ruhen paketat e derguara.
+REM Percakton network interface qe do t'e sniff RawCap.exe
+REM Ne nje host tjeter, kjo do mund te jete ndryshe.
+REM Percakton emrin e file ku ruhen paketat e derguara.
 
 	START Vertetimi.pcap
 
-REM --- Ekzekutojme file qe te shohim paketat qe kemi derguar.
-REM --- Vetem pre t'e testuar se a eshte duke funksionuar.
+REM Ekzekutojme file qe te shohim paketat qe kemi derguar.
+REM Vetem pre t'e testuar se a eshte duke funksionuar.
 
 EXIT
