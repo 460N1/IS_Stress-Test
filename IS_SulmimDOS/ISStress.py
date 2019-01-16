a7e44f76c9986804d63e5cd91cca767cc5cec793
460N1 <agon.hoxha@uni-pr.edu>
2019-01-16 19:49:41 +0100

    Slimdown

--- IS_SulmimDOS/ISStress.py
+++ IS_SulmimDOS/ISStress.py
@@ -1,9 +1,8 @@
-# /************************************************\
-# |       AUTHOR:  Agon Hoxha,  140704110004       |
-# |  SUBJECT: Internet Security,  VITI: 2018/2019  |
-# |  TASK: Developing Python application for site  |
-# |                stress testing,   #6            |
-# \************************************************/
+# /**************************\
+# |   ---  Agon Hoxha ---    |
+# |  Python application for  |
+# |     site stress test     |
+# \**************************/
 
 import argparse
 import logging
@@ -14,14 +13,14 @@ import sys
 import time
 
 parser = argparse.ArgumentParser(description="IS Stresser - simple stress test tool.", add_help=False)
-parser.add_argument('host', nargs="?", help="Target of stress test.")
+parser.add_argument('host', nargs="?", help="Target of test.")
 parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show these instructions.')
 parser.add_argument('-p', '--port', default=80, help="Target port (default 80).", type=int)
-parser.add_argument('-s', '--sockets', default=100, help="Number of sockets to use per test (default 100).", type=int)
-parser.add_argument('-d', '--detaje', dest="verbose", action="store_true", help="Enable verbose logging.")
+parser.add_argument('-s', '--sockets', default=100, help="Number of sockets to use (default 100).", type=int)
+parser.add_argument('-d', '--detaje', dest="verbose", action="store_true", help="Verbose logging.")
 parser.add_argument('-r', '--random-ua', dest="randuseragent", action="store_true", help="Use random user-agents.")
-parser.add_argument('-q', '--qete', dest="quiet", action="store_true", help="Don't show anything during execution.")
-parser.add_argument("--https", dest="https", action="store_true", help="Use HTTPS for requests.")
+parser.add_argument('-q', '--qete', dest="quiet", action="store_true", help="Don't show anything.")
+parser.add_argument("--https", dest="https", action="store_true", help="Use HTTPS.")
 
 parser.set_defaults(verbose=False)
 parser.set_defaults(randuseragent=False)
@@ -35,7 +34,7 @@ if len(sys.argv) <= 1:
     sys.exit()
 
 if args.verbose and args.quiet:
-    print("You can only use one: either -d or -q!")
+    print("Use either -d or -q!")
     sys.exit()
 else:
     if args.verbose:
@@ -63,13 +62,10 @@ user_agents = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
     "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
     "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
 ]
 
-logging.info("\nAuthor: Agon Hoxha \nVersion: 0.94 \nFinished: 15/01/2019")
+logging.info("\nAuthor: Agon Hoxha \nVersion: 1.0")
 
 def init_socket(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -91,7 +87,7 @@ def main():
     ip = args.host
     socket_count = args.sockets
     logging.info("Attacking %s with %s sockets.", ip, socket_count)
-    logging.info("Creating sockets...")
+    logging.info("Creating sockets.")
     for _ in range(socket_count):
         try:
             logging.debug("Creating socket %s", _)
@@ -121,7 +117,7 @@ def main():
             time.sleep(15)
         
         except (KeyboardInterrupt, SystemExit):
-            logging.info("Attack has stopped.")
+            logging.info("Attack was stopped.")
             break
 
 if __name__ == "__main__":
